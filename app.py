df6b93f3bcf5436dca6d60002ad5489649df931a
Abdullah1464 <muhammad.abdullah2gr8@gmail.com>
2023-10-30 12:39:47 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Home, got the /calc/func to run"
+
+@app.route("/calc/square/<x>")
+#Square function
+def square(x):
+    
+    return str(int(x) * int(x))
+
+#Sr
+@app.route("/calc/multiply/<x>/<y>")
+#Multiply Func
+def multiply(x,y):
+    
+    return str(int(x) * int(y))
+
+def sub(x):
+    return str(x - 3)
+  
+def add(x,y):
+    return x+y
+
+@app.route("/calc/div/<x>/<y>")
+def div(x,y):
+    if int(y) == 0:
+        return "Undefined (division by zero)"
+    return str(int(int(x) / int(y)))
+@app.route("/calc/mod/<x>/<y>")
+def mod(x, y):
+    """Calculate the modulus (remainder) of two numbers."""
+    if int(y) == 0:
+        return "Cannot divide by zero"
+    return str(int(x) % int(y))
---
+++ test.py
@@ -0,0 +1,12 @@
+from app import square, sub, div, multiply
+
+#Tests
+def test_square():
+    assert "4" == square(2)
+def test_sub():
+    assert "1" == sub(4)
+def test_div():
+    assert "2" == div(4,2)
+def test_multiply():
+    assert "4" == multiply(2,2)
+    
